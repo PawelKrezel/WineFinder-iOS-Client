2eb9cfd487350e22e4eb6729240d804bdf2a0369
Pawel Krezel <pawel.krezel@icloud.com>
2026-03-31 18:34:47 +0100

    Initial Commit

---
+++ WineFinder-iOS-Client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WineFinder-iOS-Client
+//
+//  Created by Pawel Krezel on 31/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WineFinder-iOS-Client/WineFinder_iOS_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineFinder_iOS_ClientApp.swift
+//  WineFinder-iOS-Client
+//
+//  Created by Pawel Krezel on 31/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WineFinder_iOS_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
